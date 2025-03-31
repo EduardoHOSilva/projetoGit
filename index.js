43f4f37f1932c70c768d25c75cad9cda6754ae57
EduardoHOSilva <eduardo.h.silva28@aluno.senai.br>
2025-03-31 15:52:03 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Minha branch");
+const numUm = parseInt(prompt("Insira um número:"));
+
+if (isNaN(numUm)){
+    //Verifica se a conversão resultou em um número válido
+    alert('Erro: Você não digitou um número válido!')
+}
+
+if(!isNaN(numUm)){
+    // Somente solicita o segundo número se o primeiro for válido
+    const numDois = parseInt(prompt("Insira outro número:"));
+
+    if (isNaN(numDois)){
+        //Verifica se a conversão resultou em um número válido
+        alert('Erro: Você não digitou um número válido!')
+    }
+
+    if(!isNaN(numUm) && !isNaN(numDois)){
+        if(numUm === numDois){
+            alert("Os números são iguais!");
+        }
+        if(numUm !== numDois){
+            alert("Os números são diferentes!");
+        }
+    }
+}
